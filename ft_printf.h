13f5ca3501874a56ddbb36c44609d6389c77d477
lmp2000 <lmrpais@gmail.com>
2025-11-13 15:15:04 +0000

    commit

---
+++ ft_printf.h
@@ -0,0 +1,21 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_printf.h                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: lude-jes <lude-jes@student.42lisboa.com    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/11/13 14:32:07 by lude-jes          #+#    #+#             */
+/*   Updated: 2025/11/13 15:00:58 by lude-jes         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef FT_PRINTF_H
+# define FT_PRINTF_H
+
+# include <stdarg.h>
+# include "libft/libft.h"
+
+int	ft_printf(const char *format, ...);
+
+#endif
