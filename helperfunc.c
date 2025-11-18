f86e4160cf9bbeea7f84c761d2d53987b6a1128b
lmp2000 <lmrpais@gmail.com>
2025-11-18 16:30:34 +0000

    commit

--- helperfunc.c
+++ helperfunc.c
@@ -6,7 +6,7 @@
 /*   By: lude-jes <lude-jes@student.42lisboa.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/11/13 22:43:15 by lude-jes          #+#    #+#             */
-/*   Updated: 2025/11/16 23:36:02 by lude-jes         ###   ########.fr       */
+/*   Updated: 2025/11/18 16:30:16 by lude-jes         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -67,32 +67,5 @@ int	handle_hex(unsigned int n, int uppercase)
 	if (n >= 16)
 		count += handle_hex(n / 16, uppercase);
 	count += handle_char(base[n % 16]);
-	return (count);
-}
-
-int	handle_hexlong(unsigned long n)
-{
-	char	*base;
-	int		count;
-
-	base = "0123456789abcdef";
-	count = 0;
-	if (n >= 16)
-		count += handle_hexlong(n / 16);
-	count += handle_char(base[n % 16]);
-	return (count);
-}
-
-int	handle_ptr(void *ptr)
-{
-	unsigned long	address;
-	int				count;
-
-	if (!ptr)
-		return (handle_string("(nil)"));
-	count = 0;
-	address = (unsigned long)ptr;
-	count += handle_string("0x");
-	count += handle_hexlong(address);
 	return (count);
 }
---
+++ helperfunc2.c
@@ -0,0 +1,41 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   helperfunc2.c                                      :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: lude-jes <lude-jes@student.42lisboa.com    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/11/18 16:29:29 by lude-jes          #+#    #+#             */
+/*   Updated: 2025/11/18 16:29:43 by lude-jes         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft/libft.h"
+#include "ft_printf.h"
+
+int	handle_hexlong(unsigned long n)
+{
+	char	*base;
+	int		count;
+
+	base = "0123456789abcdef";
+	count = 0;
+	if (n >= 16)
+		count += handle_hexlong(n / 16);
+	count += handle_char(base[n % 16]);
+	return (count);
+}
+
+int	handle_ptr(void *ptr)
+{
+	unsigned long	address;
+	int				count;
+
+	if (!ptr)
+		return (handle_string("(nil)"));
+	count = 0;
+	address = (unsigned long)ptr;
+	count += handle_string("0x");
+	count += handle_hexlong(address);
+	return (count);
+}
